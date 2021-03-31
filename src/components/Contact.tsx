71ce16efb21d09e3a102cbce98d1d8d98c7679cb
Vaasu Dhand <dhandvaasu@gmail.com>
2021-03-31 21:43:21 +0530

    Form Added

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,6 +4,35 @@ export default function Contact() {
   return (
     <section id="contact">
       <h1>Contact</h1>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        {/* <p>
+          <label>
+            Your Role:{' '}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p> */}
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </section>
   );
 }
