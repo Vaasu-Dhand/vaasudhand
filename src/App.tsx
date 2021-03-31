57ee3bb6d6d13a9b95dde86031feb700ba01c05e
Vaasu Dhand <dhandvaasu@gmail.com>
2021-03-31 18:44:10 +0530

    Skills.tsx Functionality Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { Nav, Hero, Skills, Projects, Contact, Footer } from './components'
+import React from 'react';
+import { Nav, Hero, Skills, Projects, Contact, Footer } from './components';
+import { ViewportProvider } from './hooks/useViewport';
 
 function App() {
-
   return (
     <div className="App">
       <Nav />
       <Hero />
-      <Skills />
+      <ViewportProvider>
+        <Skills />
+      </ViewportProvider>
       <Projects />
       <Contact />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { map } from 'lodash';
+import React, { useEffect, useState } from 'react';
+import { useViewport, Viewport as deviceTypes } from '../hooks/useViewport';
+import { map, size } from 'lodash';
 import {
   HTML5,
   CSS3,
@@ -22,7 +23,8 @@ import {
 } from '../svg/';
 
 export default function Skills() {
-  const skills = {  // ReArrange Skills
+  const skills = {
+    // ReArrange Skills
     HTML5,
     CSS3,
     SASS,
@@ -43,13 +45,52 @@ export default function Skills() {
     ELIXIR,
   };
 
+  // Hooks
+  const deviceType = useViewport(); // * Returns the DeviceType Depending on the Width of Viewport
+
+  const [skillsToBeDisplayed, setSkillsToBeDisplayed] = useState({})
+
+  useEffect(() => {
+    // console.log("Screen Size Changed!", {deviceType});
+    let numOfSkillsRemoved;
+    switch (deviceType) {
+      case deviceTypes.DESKTOP:
+        numOfSkillsRemoved = 2;
+        break;
+      case deviceTypes.WIDE:
+        numOfSkillsRemoved = 2;
+        break;
+      case deviceTypes.PHABLET:
+        numOfSkillsRemoved = 0;
+        break;
+      case deviceTypes.PHONE:
+        numOfSkillsRemoved = 0;
+        break;
+      case deviceTypes.TABLET:
+        numOfSkillsRemoved = 3;
+        break;
+      case deviceTypes.LAPTOP:
+        numOfSkillsRemoved = 4;
+        break;
+      default:
+        console.warn('Weird! Screen Size Switch in Default');
+        numOfSkillsRemoved = 0;
+        break;
+    }
+    
+    // * Slice Skills Depending on Screen Size and Store it in State
+    const slicedSkills = Object.fromEntries(Object.entries(skills).slice(0, size(skills) - numOfSkillsRemoved));
+    setSkillsToBeDisplayed(slicedSkills)
+    
+  }, [useViewport()]);
+
   return (
     <section id="skills" className="container">
       <h1>My Toolkit</h1>
       <ul className="wrapper-skills">
-        {map(skills, (Skill, key) =>  (
-          <li>
-            <Skill key={key} className={key.toLowerCase()} />
+        {map(skillsToBeDisplayed, (Skill: React.ElementType, key) => (
+          <li key={key}>
+            <Skill className={key.toLowerCase()} />
             <h3>{key}</h3>
           </li>
         ))}
