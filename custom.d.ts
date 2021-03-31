b6fbc858aaf52c4aeb69a9d56a89cd59c6ed84be
Vaasu Dhand <dhandvaasu@gmail.com>
2021-03-31 22:20:11 +0530

    Netlify Build Error Fix#3

--- custom.d.ts
+++ custom.d.ts
@@ -1,11 +1,11 @@
-// Fix typescript error for plugin vite-plugin-svgr
-declare module '*.svg' {
-  import * as React from 'react';
+// // Fix typescript error for plugin vite-plugin-svgr
+// declare module '*.svg' {
+//   import * as React from 'react';
 
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
+//   export const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
 
-  const src: string
-  export default src
-}
+//   const src: string
+//   export default src
+// }
