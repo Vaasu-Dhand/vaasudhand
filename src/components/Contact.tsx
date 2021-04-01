83f5a3ddd9060298011758efc43ef3e4368e5a7a
Vaasu Dhand <dhandvaasu@gmail.com>
2021-04-02 00:32:09 +0530

    Added Subject in Form + Build Fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -14,13 +14,14 @@ export default function Contact() {
   const [formState, setFormState] = useState({
     name: '',
     email: '',
+    subject: '',
     message: '',
   });
   const [failure, setFailure] = useState(false);
   const [success, setSuccess] = useState(false);
 
-  const initialState = { name: '', email: '', message: '' };
-  const { name, email, message } = formState;
+  const initialState = { name: '', email: '', subject: '', message: '' };
+  const { name, email, subject, message } = formState;
 
   // Event Handlers
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
@@ -81,70 +82,86 @@ export default function Contact() {
         </p>
       </form> */}
       <form onSubmit={handleSubmit}>
-      {success && 'Thank you, form has been submitted!'}
+        {success && 'Thank you, form has been submitted!'}
         {failure && 'Sorry, Something went wrong!'}
-      <div className="background">
-        <div className="container">
-          <div className="screen">
-            <div className="screen-header">
-              <div className="screen-header-left">
-                <div className="screen-header-button close"></div>
-                <div className="screen-header-button maximize"></div>
-                <div className="screen-header-button minimize"></div>
-              </div>
-              <div className="screen-header-right">
-                <div className="screen-header-ellipsis"></div>
-                <div className="screen-header-ellipsis"></div>
-                <div className="screen-header-ellipsis"></div>
-              </div>
-            </div>
-            <div className="screen-body">
-              <div className="screen-body-item left">
-                <div className="app-title">
-                  <span>CONTACT</span>
-                  <span>ME</span>
+        <div className="background">
+          <div className="container">
+            <div className="screen">
+              <div className="screen-header">
+                <div className="screen-header-left">
+                  <div className="screen-header-button close"></div>
+                  <div className="screen-header-button maximize"></div>
+                  <div className="screen-header-button minimize"></div>
                 </div>
-                {/* <div className="app-contact">
+                <div className="screen-header-right">
+                  <div className="screen-header-ellipsis"></div>
+                  <div className="screen-header-ellipsis"></div>
+                  <div className="screen-header-ellipsis"></div>
+                </div>
+              </div>
+              <div className="screen-body">
+                <div className="screen-body-item left">
+                  <div className="app-title">
+                    <span>CONTACT</span>
+                    <span>ME</span>
+                  </div>
+                  {/* <div className="app-contact">
                   CONTACT INFO : +62 81 314 928 595
                 </div> */}
-              </div>
-              <div className="screen-body-item">
-                <div className="app-form">
-                  <div className="app-form-group">
-                    <input
-                      className="app-form-control"
-                      placeholder="NAME"
-                      type="text"
-                      name="name"
-                      value={name}
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className="app-form-group">
-                    <input className="app-form-control" placeholder="EMAIL" type="email"
-              name="email"
-              value={email}
-              onChange={handleChange} />
-                  </div>
-                  <div className="app-form-group">
-                    <input
-                      className="app-form-control"
-                      placeholder="CONTACT NO"
-                    />
-                  </div>
-                  <div className="app-form-group message">
-                    <textarea className="app-form-control" placeholder="MESSAGE" name="message" value={message} onChange={handleChange} />
-                  </div>
-                  <div className="app-form-group buttons">
-                    <button className="app-form-button">CLEAR</button>
-                    <button className="app-form-button" type="submit">SEND</button>
+                </div>
+                <div className="screen-body-item">
+                  <div className="app-form">
+                    <div className="app-form-group">
+                      <input
+                        className="app-form-control"
+                        placeholder="NAME"
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="app-form-group">
+                      <input
+                        className="app-form-control"
+                        placeholder="EMAIL"
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="app-form-group">
+                      <input
+                        className="app-form-control"
+                        placeholder="SUBJECT"
+                        type="text"
+                        name="subject"
+                        value={subject}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="app-form-group message">
+                      <textarea
+                        className="app-form-control"
+                        placeholder="MESSAGE"
+                        name="message"
+                        value={message}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="app-form-group buttons">
+                      <button className="app-form-button">CLEAR</button>
+                      <button className="app-form-button" type="submit">
+                        SEND
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
       </form>
     </section>
   );
