407ed180533775091e77698f21e61f5599201920
Vaasu Dhand <dhandvaasu@gmail.com>
2021-04-01 15:37:17 +0530

    Contact Form New Styles

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -18,10 +18,10 @@ export default function Contact() {
   });
   const [failure, setFailure] = useState(false);
   const [success, setSuccess] = useState(false);
-  
+
   const initialState = { name: '', email: '', message: '' };
   const { name, email, message } = formState;
-  
+
   // Event Handlers
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     try {
@@ -44,35 +44,98 @@ export default function Contact() {
   > = (e) => setFormState({ ...formState, [e.target.name]: e.target.value });
 
   return (
-    <form onSubmit={handleSubmit}>
-      {success && 'Thank you, form has been submitted!'}
-      {failure && 'Sorry, Something went wrong!'}
-      <p>
-        <label>
-          Your Name:{' '}
-          <input type="text" name="name" value={name} onChange={handleChange} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email:{' '}
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleChange}
-          />
-        </label>
-      </p>
-      <p>
-        <label>
-          Message:{' '}
-          <textarea name="message" value={message} onChange={handleChange} />
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <section id="contact">
+      {/* <form onSubmit={handleSubmit}>
+        {success && 'Thank you, form has been submitted!'}
+        {failure && 'Sorry, Something went wrong!'}
+        <p>
+          <label>
+            Your Name:{' '}
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email:{' '}
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:{' '}
+            <textarea name="message" value={message} onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form> */}
+      <div className="background">
+        <div className="container">
+          <div className="screen">
+            <div className="screen-header">
+              <div className="screen-header-left">
+                <div className="screen-header-button close"></div>
+                <div className="screen-header-button maximize"></div>
+                <div className="screen-header-button minimize"></div>
+              </div>
+              <div className="screen-header-right">
+                <div className="screen-header-ellipsis"></div>
+                <div className="screen-header-ellipsis"></div>
+                <div className="screen-header-ellipsis"></div>
+              </div>
+            </div>
+            <div className="screen-body">
+              <div className="screen-body-item left">
+                <div className="app-title">
+                  <span>CONTACT</span>
+                  <span>US</span>
+                </div>
+                {/* <div className="app-contact">
+                  CONTACT INFO : +62 81 314 928 595
+                </div> */}
+              </div>
+              <div className="screen-body-item">
+                <div className="app-form">
+                  <div className="app-form-group">
+                    <input
+                      className="app-form-control"
+                      placeholder="NAME"
+                      value="Krisantus Wanandi"
+                    />
+                  </div>
+                  <div className="app-form-group">
+                    <input className="app-form-control" placeholder="EMAIL" />
+                  </div>
+                  <div className="app-form-group">
+                    <input
+                      className="app-form-control"
+                      placeholder="CONTACT NO"
+                    />
+                  </div>
+                  <div className="app-form-group message">
+                    <input className="app-form-control" placeholder="MESSAGE" />
+                  </div>
+                  <div className="app-form-group buttons">
+                    <button className="app-form-button">CANCEL</button>
+                    <button className="app-form-button">SEND</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
