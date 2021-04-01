716d98de33d59aa99db34289460b4e4b32055b37
Vaasu Dhand <dhandvaasu@gmail.com>
2021-04-01 11:52:38 +0530

    Netlify Form Test

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,50 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEventHandler, FormEventHandler, useState } from 'react'
 
 export default function Contact() {
 
-  const [success, setSuccess] = useState(false);
+  const encode = (data: any) => { // ? I've set any type explicilty
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
 
-  useEffect(() => {
-    if ( window.location.search.includes('success=true') ) {
-      setSuccess(true);
-    }
-  }, []);
+  // const [name, setName] = useState("")
+  // const [email, setEmail] = useState("")
+  // const [message, setMessage] = useState("")
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
 
-  return (
-    <section id="contact">
-      <h1>Contact</h1>
-      {success && (
-          <p style={{ color: "green" }}>Thanks for your message! </p>
-        )}
-      <form name="contact" method="POST" data-netlify="true">
+  /* Here’s the juicy bit for posting the form submission */
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...formState })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  const handleChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = (e) => setFormState({ ...formState, [e.target.name]: e.target.value });
+
+    const { name, email, message } = formState;
+    return (
+      <form onSubmit={handleSubmit}>
         <p>
           <label>
-            Your Name: <input type="text" name="name" />
+            Your Name: <input type="text" name="name" value={name} onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
-            Your Email: <input type="email" name="email" />
+            Your Email: <input type="email" name="email" value={email} onChange={handleChange} />
           </label>
         </p>
-        {/* <p>
-          <label>
-            Your Role:{' '}
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p> */}
         <p>
           <label>
-            Message: <textarea name="message"></textarea>
+            Message: <textarea name="message" value={message} onChange={handleChange} />
           </label>
         </p>
         <p>
           <button type="submit">Send</button>
         </p>
       </form>
-    </section>
-  );
-}
+    );
+  }
+
