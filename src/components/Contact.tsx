603713fcae35f562d91164c3c1ea88cc63ac52c3
Vaasu Dhand <dhandvaasu@gmail.com>
2021-04-01 12:56:51 +0530

    Zappier Integrated

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,60 +1,78 @@
-import React, { ChangeEventHandler, FormEventHandler, useState } from 'react'
+import React, { ChangeEventHandler, FormEventHandler, useState } from 'react';
 
 export default function Contact() {
-
-  const encode = (data: any) => { // ? I've set any type explicilty
+  const encode = (data: any) => {
+    // ? I've set any type explicilty
     return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-  }
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&');
+  };
 
-  // const [name, setName] = useState("")
-  // const [email, setEmail] = useState("")
-  // const [message, setMessage] = useState("")
+  // Hooks
   const [formState, setFormState] = useState({
     name: '',
     email: '',
-    message: ''
-  })
-
-  /* Here’s the juicy bit for posting the form submission */
-
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...formState })
-    })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
-
-    e.preventDefault();
+    message: '',
+  });
+  const [failure, setFailure] = useState(false);
+  const [success, setSuccess] = useState(false);
+  
+  const initialState = { name: '', email: '', message: '' };
+  const { name, email, message } = formState;
+  
+  // Event Handlers
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    try {
+      e.preventDefault();
+      await fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode({ 'form-name': 'contact', ...formState }),
+      });
+      setSuccess(true);
+      setFormState(initialState); // * State is not clearing up!
+    } catch (error) {
+      console.error(error);
+      setFailure(true);
+    }
   };
 
-  const handleChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = (e) => setFormState({ ...formState, [e.target.name]: e.target.value });
+  const handleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (e) => setFormState({ ...formState, [e.target.name]: e.target.value });
 
-    const { name, email, message } = formState;
-    return (
-      <form onSubmit={handleSubmit}>
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" value={name} onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" value={email} onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message" value={message} onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    );
-  }
-
+  return (
+    <form onSubmit={handleSubmit}>
+      {success && 'Thank you, form has been submitted!'}
+      {failure && 'Sorry, Something went wrong!'}
+      <p>
+        <label>
+          Your Name:{' '}
+          <input type="text" name="name" value={name} onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email:{' '}
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message:{' '}
+          <textarea name="message" value={message} onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  );
+}
